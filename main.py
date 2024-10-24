f32a46a1796c383499a48052e59a7ce1fed547ef
Mariame Chibani <soundouce.chibani@gmail.com>
2024-10-24 20:04:19 +0200

    added feature to fetch trending recipes from Allrecipes when no keyword is entered

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from recipe_scrapper import Recipe_scrapper
 headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
-recipe_scp = Recipe_scrapper('chicken')
-recipe_scp.collect_recipes()
-r = recipe_scp.get_recipe()
+user_input = int(input("""
+                    1. Cherchez une recette 
+                    2. Recette tendance 
+"""))
+if user_input == 1: 
+    recipe_scp = Recipe_scrapper(user_input,'chicken')
+    recipe_scp.collect_recipes()
+    r = recipe_scp.get_recipe("Chicken Fried Chicken")
+
+else : 
+    recipe_scp = Recipe_scrapper(user_input)
+    recipe_scp.collect_recipes()
+
+    
 recipe_scp.close()
--- recipe_scrapper.py
+++ recipe_scrapper.py
@@ -8,14 +8,20 @@ from recipe import Recipe
 
 class Recipe_scrapper():
 
-    BASE_URL = f'https://www.allrecipes.com/search?'
+    BASE_URL = 'https://www.allrecipes.com'
     recipes_links = []
-    def __init__(self, keyword) -> None:
+    def __init__(self,search_mode,keyword=""):
         self.keyword = keyword
         self.driver = self._init_driver()
-        self.page_url = f'{self.BASE_URL}?{self.keyword}={self.keyword}&offset=1&q={self.keyword}'
+        self.init_research(search_mode)
         self._init_driver()
-        
+
+    def init_research(self, search_mode): 
+        if search_mode == 1: 
+            self.page_url= f'{self.BASE_URL}/search?{self.keyword}={self.keyword}&offset=1&q={self.keyword}'
+        else: 
+            self.page_url = self.BASE_URL
+
     def _init_driver(self):        
         service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service)
@@ -32,26 +38,35 @@ class Recipe_scrapper():
                 }
             )
         print(self.recipes_links)
-    
-    def get_recipe_obj(self, url) -> str: 
+
+    def is_recipe(self)-> int: 
+        try: 
+            self.driver.find_element(By.CLASS_NAME, 'mm-recipes-structured-ingredients__list') 
+            return 0 
+        except: 
+            print(f"'{self.driver.find_element(By.CSS_SELECTOR, 'h1').text}' is not a recipe! Sorry")
+            return -1
+
+    def get_recipe_obj(self, url): 
         self.driver.get(url)
-        recipe = {
-            'title': self.driver.find_element(By.TAG_NAME, 'h1').text, 
-            'nb_star': self.driver.find_element(By.CSS_SELECTOR, '#mm-recipes-review-bar__rating_1-0').text, 
-            'description': self.driver.find_element(By.CSS_SELECTOR, ".article-subheading.type--dog").text
-        }
+        if self.is_recipe()!= -1: 
+            recipe = {
+                'title': self.driver.find_element(By.TAG_NAME, 'h1').text, 
+                'nb_star': self.driver.find_element(By.CSS_SELECTOR, '#mm-recipes-review-bar__rating_1-0').text, 
+                'description': self.driver.find_element(By.CSS_SELECTOR, ".article-subheading.type--dog").text
+            }
 
-        infos = self.driver.find_elements(By.CSS_SELECTOR, '.mm-recipes-details__value') 
-        recipe['info_prep'] = {['prep_time', 'cook_time', 'total_time', 'servings','yield'][i]: infos[i].text for i in range(len(infos))}
+            infos = self.driver.find_elements(By.CSS_SELECTOR, '.mm-recipes-details__value') 
+            recipe['info_prep'] = {['prep_time', 'cook_time', 'total_time', 'additional_time','servings','yield'][i]: infos[i].text for i in range(len(infos))}
 
-        recipe['ingredients_list'] = [i.text for i in self.driver.find_element(By.CLASS_NAME, 'mm-recipes-structured-ingredients__list').find_elements(By.TAG_NAME, "p")]
+            recipe['ingredients_list'] = [i.text for i in self.driver.find_element(By.CLASS_NAME, 'mm-recipes-structured-ingredients__list').find_elements(By.TAG_NAME, "p")]
 
-        recipe['direction']= [i.text for i in self.driver.find_element(By.ID, 'mm-recipes-steps__content_1-0').find_elements(By.CSS_SELECTOR,'li > p')]
+            recipe['direction']= [i.text for i in self.driver.find_element(By.ID, 'mm-recipes-steps__content_1-0').find_elements(By.CSS_SELECTOR,'li > p')]
 
-        nutrition_fact = self.driver.find_elements(By.CLASS_NAME , 'mm-recipes-nutrition-facts-summary__table-cell.type--dog-bold')
-        recipe['nutrition_fact'] = {['calories', 'fat', 'carbs','protein'][i]: nutrition_fact[i].text for i in range(len(nutrition_fact))}
+            nutrition_fact = self.driver.find_elements(By.CLASS_NAME , 'mm-recipes-nutrition-facts-summary__table-cell.type--dog-bold')
+            recipe['nutrition_fact'] = {['calories', 'fat', 'carbs','protein'][i]: nutrition_fact[i].text for i in range(len(nutrition_fact))}
 
-        return recipe
+            return recipe
     
     def collect_recipe_by_idx(self, idx): 
         page_url = self.recipes_links[idx].get('url') 
@@ -59,24 +74,27 @@ class Recipe_scrapper():
         
 
     def get_idx_of_recipe(self, recipe_title) -> int : 
-        try: 
-            self.recipes_links.index(recipe_title)
-        except:
-            print("erreur")
+        for index, recipe in enumerate(self.recipes_links):
+            if recipe['title'] == recipe_title:
+                return index
+        print(f"Recipe titled '{recipe_title}' not found.")
+        return -1
 
-    def get_recipe(self): 
-        page_url = self.collect_recipe_by_idx(0) #change idx to a modifiable value 
-        recipe_obj = self.get_recipe_obj(page_url)
+    def get_recipe(self,recipe_title): 
+        idx = self.get_idx_of_recipe(recipe_title)
+        if idx != -1 : 
+            page_url = self.collect_recipe_by_idx(idx) 
+            recipe_obj = self.get_recipe_obj(page_url)
+            if recipe_obj:
+                recipe = Recipe(recipe_obj.get('title'), recipe_obj.get('nb_star'), 
+                                recipe_obj.get('description'), recipe_obj.get('info_prep').get('prep_time'), 
+                                recipe_obj.get('info_prep').get('cook_time'), recipe_obj.get('info_prep').get('servings'),
+                                recipe_obj.get('ingredients_list'), recipe_obj.get('direction'), 
+                                recipe_obj.get('nutrition_fact').get('calories'), recipe_obj.get('nutrition_fact').get('fat'), 
+                                recipe_obj.get('nutrition_fact').get('carbs'), recipe_obj.get('nutrition_fact').get('protein')
+                                )
+                print(recipe)
+                return recipe
         
-        recipe = Recipe(recipe_obj.get('title'), recipe_obj.get('nb_star'), 
-                        recipe_obj.get('description'), recipe_obj.get('info_prep').get('prep_time'), 
-                        recipe_obj.get('info_prep').get('cook_time'), recipe_obj.get('info_prep').get('servings'),
-                        recipe_obj.get('ingredients_list'), recipe_obj.get('direction'), 
-                        recipe_obj.get('nutrition_fact').get('calories'), recipe_obj.get('nutrition_fact').get('fat'), 
-                        recipe_obj.get('nutrition_fact').get('carbs'), recipe_obj.get('nutrition_fact').get('protein')
-                        )
-        print(recipe)
-        return recipe
-    
     def close(self): 
         self.driver.quit() 
